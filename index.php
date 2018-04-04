388e440e142c135ffccf95ab1b3387a02669ee91
CoolDeep eVagabond <kskhadka@gmail.com>
2018-04-04 16:06:28 +0545

    complted music you  might like page

--- index.php
+++ index.php
@@ -11,13 +11,13 @@
     while($row = mysqli_fetch_array($albumQuery)) {
       
       echo "<div class='gridViewItem'>
-
-              <img src='" . $row['artworkPath'] . "'>
-
-              <div class='gridViewInfo'>"
-                .$row['title']. 
+              <a href='album.php?id=" . $row['id'] . "'>
+                <img src='" . $row['artworkPath'] . "'>              
+                
+                <div class='gridViewInfo'>"
+                  .$row['title']. 
                 "</div>
-                      
+              </a>                      
             </div>";
           }
     ?>
